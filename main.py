f7a270fd7a34d3752944b0ffd28178f004e5da1c
Martin Thuresson <martin.thure@gmail.com>
2019-08-11 09:37:41 +0200

    Apply with emial

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from bottle import post, run, get, template
+from bottle import post, run, get, template, request
 import requests
 import sqlite3
 import tables
+import random
+import ConfigParser
+config = ConfigParser.ConfigParser()
+config.read('../BESK.ini')
 
-db = sqlite3.connect("../BESK.db").cursor()
+db = sqlite3.connect("../BESK.db")
+cursor = db.cursor()
 
 for table in tables.tables:
   db.execute(table)
 
+def send_email(to, subject, message):
+   url = config.get('general','email_url')
+   json_data = {"to":to,"subject":subject,"message":message,"key":config.get('general','email_key')}
+   requests.post(url, json=json_data)
 # Apply
 @get('/apply')
 def apply():
-    return template("apply.html")
+    if request.query.get('barn') and request.query.get('vuxna'):
+      return template("apply.html",
+        barn=int(request.query.get('barn')),
+        vuxna=int(request.query.get('vuxna')),
+        kodstugor=[{"id":1,"namn":"Kodstuga1"},{"id":2,"namn":"Kodstuga2"}])
+    else:
+      return template("apply_step1.html")
 
 # Apply
 @post('/apply')
 def apply():
-    return template("apply.html")
-
+    barnid  = []
+    vuxid = []
+    for i, value in enumerate(request.forms.getall("barn_efternamn")):
+        params = (
+        request.forms.get("kodstuga"),
+        request.forms.getall("barn_fornamn")[i],
+        request.forms.getall("barn_efternamn")[i],
+        request.forms.getall("kon")[i],
+        request.forms.getall("skola")[i]
+        )
+        barnid.append(params[0])
+        cursor.execute("INSERT INTO deltagare (kodstugor_id,fornamn,efternamn,kon,skola) VALUES (?,?,?,?,?)",params)
+    db.commit()
+    for i, value in enumerate(request.forms.getall("vuxen_efternamn")):
+        params = (
+        request.forms.getall("vuxen_fornamn")[i],
+        request.forms.getall("vuxen_efternamn")[i],
+        request.forms.getall("email")[i],
+        request.forms.getall("telefon")[i]
+        )
+        vuxid.append(params[0])
+        cursor.execute("INSERT INTO kontaktpersoner (fornamn,efternamn,epost,telefon) VALUES (?,?,?,?)",params)
+    db.commit()
+    hittade = (request.forms.get("hittade"),)
+    cursor.execute("INSERT INTO hittade (hittade) VALUES (?)",hittade)
+    db.commit()
+    for vid in vuxid:
+      for bid in vuxid:
+        cursor.execute("INSERT INTO kontaktpersoner_deltagare (kontaktpersoner_id, deltagare_id) VALUES (?,?)",(vid,bid))
+    db.commit()
+    for email in request.forms.getall("email"):
+        send_email(email, "Tack för din intresseanmälan" , "Hej!\n\nVi har mottagit din intresseanmälan och kommer att återkomma när urvalet till kostugan är klart.\n\nMvh Kodcentrum")
+    return template("apply_step2.html")
 # Confirm
 @post('/confirm')
 def apply():
@@ -30,4 +76,4 @@ def apply():
 def apply():
     return "Hello World!"
 
-run(host='localhost', port=8080, reloader=True)
+run(host='localhost', port=8080)
--- tables.py
+++ tables.py
@@ -2,86 +2,89 @@
 
 tables = ['''
   CREATE TABLE IF NOT EXISTS kodstugor (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   namn TEXT,
   sms_text TEXT,
   epost_text TEXT,
-  epost_rubrik TEXT ) WITHOUT ROWID;
+  epost_rubrik TEXT );
+''',
+'''
+  CREATE TABLE IF NOT EXISTS hittade (
+  hittade TEXT);
 ''',
 '''
 CREATE TABLE IF NOT EXISTS kodstugor_datum (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   kodstugor_id INT,
   datum TEXT,
   typ TEXT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id)
-  ) WITHOUT ROWID; 
+  ); 
 ''',
 '''
 CREATE INDEX IF NOT EXISTS kodstugor_datum_kodstugor_id ON kodstugor_datum(kodstugor_id);
 ''',
 '''
 CREATE TABLE IF NOT EXISTS deltagare (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   kodstugor_id INT,
   fornamn TEXT,
   efternamn TEXT,
   kon TEXT,
+  skola TEXT,
   foto TEXT,
   framnummer TEXT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id)
-  ) WITHOUT ROWID; 
+  ); 
 ''',
 '''
 CREATE TABLE IF NOT EXISTS kontaktpersoner (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   fornamn TEXT,
   efternamn TEXT,
-  epost TEXT UNIQUE,
-  telefon TEXT) WITHOUT ROWID;
+  epost TEXT,
+  telefon TEXT);
 ''',
 '''
 CREATE TABLE IF NOT EXISTS kontaktpersoner_deltagare (
-  id INT PRIMARY KEY,
-  kodstugor_id INT,
+  id INTEGER PRIMARY KEY,
   kontaktpersoner_id INT,
   deltagare_id INT,
-  FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id),
   FOREIGN KEY(kontaktpersoner_id) REFERENCES kontaktpersoner(id),
   FOREIGN KEY(deltagare_id) REFERENCES deltagare(id)
-  ) WITHOUT ROWID; 
+  ); 
 ''',
 '''
 CREATE TABLE IF NOT EXISTS deltagande (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   kodstugor_id INT,
   deltagare_id INT,
   status TEXT,
   datum TEXT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id),
   FOREIGN KEY(deltagare_id) REFERENCES deltagare(id)
-  ) WITHOUT ROWID; 
+  ); 
 ''',
 '''
 CREATE TABLE IF NOT EXISTS kick_svar (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   deltagare_id INT,
   svar TEXT,
   datum TEXT,
   FOREIGN KEY(deltagare_id) REFERENCES deltagare(id)
-  ) WITHOUT ROWID; 
+  ); 
 ''',
 '''
 CREATE TABLE IF NOT EXISTS sms_svar (
-  id TEXT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   fran TEXT,
   till TEXT,
   text TEXT,
-  datum TEXT) WITHOUT ROWID; 
+  datum TEXT); 
 ''',
 '''
 CREATE TABLE IF NOT EXISTS utskick (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   kodstugor_id INT,
   typ TEXT,
   rubrik TEXT,
@@ -89,36 +92,36 @@ CREATE TABLE IF NOT EXISTS utskick (
   datum TEXT,
   status TEXT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id)
-  ) WITHOUT ROWID;
+  );
 ''',
 '''
 CREATE INDEX IF NOT EXISTS utskick_kodstugor_id ON utskick(kodstugor_id);
 ''',
 '''
 CREATE TABLE IF NOT EXISTS volontarer (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   kodstugor_id INT,
-  epost TEXT UNIQUE,
+  epost TEXT  UNIQUE,
   namn TEXT,
   telefon TEXT,
   password TEXT,
   session TEXT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id)
-  ) WITHOUT ROWID;
+  );
 ''',
 '''
 CREATE INDEX IF NOT EXISTS volontarer_session ON volontarer(session);
 ''',
 '''
 CREATE TABLE IF NOT EXISTS volontarer_plannering (
-  id INT PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   datum TEXT,
   volontarer_id INT,
   status TEXT,
   kodstugor_id INT,
   FOREIGN KEY(kodstugor_id) REFERENCES kodstugor(id),
   FOREIGN KEY(volontarer_id) REFERENCES volontarer(id)
-  ) WITHOUT ROWID;
+  );
 ''',
 '''
 CREATE INDEX IF NOT EXISTS volontärer_plannering_kodstugor_id ON volontarer_plannering(kodstugor_id);
