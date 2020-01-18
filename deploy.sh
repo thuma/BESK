f506afb4f64749c49cb84d608ff7747d9186ad69
Martin Thuresson <martin.thure@gmail.com>
2020-01-18 15:27:25 +0100

    Added deplay script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+pytest
+if [ $? -eq 1 ]
+then
+    echo "Ingen deploy p.g.a. fallerade tester"
+else
+    ssh besk.kodcentrum.se -l besk -A "cd BESK; git pull; sudo systemctl restart besk; sudo systemctl status besk;"
+fi
