d24b6624bb526fbb013ec4e9d09493b8d036f8c5
Samuel Genheden <samuel.genheden@gmail.com>
2020-05-01 16:05:28 +0200

    Fix tests for datum utskick

--- tests/api_data.py
+++ tests/api_data.py
@@ -2,108 +2,106 @@
 # -*- coding: utf-8 -*-
 import arrow
 from uuid import uuid4
-from copy import deepcopy
 from random import randint
-
-post_typer = {
-    "kodstuga": {
-        "namn": "Test Kodstuga",
-        "sms_text": "Påminnelse om kodstuga.",
-        "sms_status": "aktiv",
-        "epost_text": "Påminnelse om kodstuga.",
-        "epost_rubrik": "Påminnelse om kodstuga",
-        "epost_status": "aktiv",
-        "epost_text_ja": "Välkommen",
-        "epost_rubrik_ja": "Du har nu tackat ja.",
-        "epost_status_ja": "aktiv",
-        "typ": "Kodstuga",
-        "open": "Ja"
-    },
-    "volontar": {
-        "namn": "Test Volontär",
-        "epost": uuid4().hex+"@none.com",
-        "telefon": "0723175800",
-        "kodstugor_id": None,
-        "roller": "volontär"
-    },
-    "datum": {
-        "kodstugor_id": None,
-        "datum": [
-            arrow.utcnow().to('Europe/Stockholm').format('YYYY-MM-DD'),
-            arrow.utcnow().shift(hours=24).to('Europe/Stockholm').format('YYYY-MM-DD'),
-            arrow.utcnow().shift(hours=48).to('Europe/Stockholm').format('YYYY-MM-DD')
-        ],
-        "typ": [
-            "kodstuga",
-            "kodstuga",
-            "kodstuga"
-        ]
-    },
-    "utskick": {
-        "kodstugor_id": None,
-        "datum": arrow.utcnow().format('YYYY-MM-DD'),
-        "typ": "e-post",
-        "status": "aktiv",
-        "rubrik": "TEST_ATT_RADERA",
-        "text": "Meddelnande som skickas till kontaktpersonen detta datum."
-    },
-    "kontaktperson": {
-        "fornamn": "KP Fornamn",
-        "efternamn": "KP Efternamn",
-        "epost": uuid4().hex+"@test.com",
-        "telefon": "07231"+str(randint(10000, 99990))
-    },
-    "deltagare": {
-        "fornamn": "DT Fornamn",
-        "efternamn": "DT Efternamn",
-        "status": "ja",
-        "kon": "han",
-        "foto": "ja",
-        "klass": "åk 4",
-        "skola": "Stenåken",
-        "kodstuga": None,
-        "skonto": "",
-        "slosen": ""
-    },
-    "apply": {
-        "kodstuga": None,
-        "barn_fornamn": ["Test_deltagare_to_be_deleted", "Test2_deltagare_to_be_deleted"],
-        "barn_efternamn": ["Thuresson", "test1a"],
-        "klass": ["åk 5", "åk 4"],
-        "skola": ["Test", "Test"],
-        "kon": ["hon", "hen"],
-        "vuxen_fornamn": ["sdfsdf", "jh"],
-        "vuxen_efternamn": ["vbhjh", "vbhjh"],
-        "email": [uuid4().hex+"@test.com", uuid4().hex+"@test.com"],
-        "telefon": ["07231"+str(randint(10000, 99990)), "07231"+str(randint(1000, 99990))],
-        "hittade": "id2",
-        "approve": "ja"
-    },
-    "apply_admin": {
-        "kodstuga": None,
-        "barn_fornamn": ["Test Deltagare Förnamn", "Test Deltagare Förnamn 2"],
-        "barn_efternamn": ["Test Deltagare Efternamn", "Test Deltagare Efternamn 2"],
-        "klass": ["åk 5", "åk 4"],
-        "skola": ["Test", "Test"],
-        "foto": ["ja", "nej"],
-        "kon": ["hon", "hen"],
-        "vuxen_fornamn": ["sdfsdf", "jh"],
-        "vuxen_efternamn": ["vbhjh", "vbhjh"],
-        "email": [uuid4().hex+"@test.com", uuid4().hex+"@test.com"],
-        "telefon": ["07231"+str(randint(10000, 99990)), "07231"+str(randint(1000, 99990))],
-        "hittade": "id2",
-        "approve": "ja",
-        "invite_now": "ja"
-    },
-    "volontarer_plannering": {
-        "volontarer_id": None,
-        "kodstugor_id": None,
-        "datum": arrow.utcnow().format('YYYY-MM-DD'),
-        "status": "ja",
-        "kommentar": "Meddelande"
-    }
-}
 
 
 def make_data(namn, extra_data={}):
-    return {**deepcopy(post_typer[namn]), **extra_data}
+    post_typer = {
+        "kodstuga": {
+            "namn": "Test Kodstuga",
+            "sms_text": "Påminnelse om kodstuga.",
+            "sms_status": "aktiv",
+            "epost_text": "Påminnelse om kodstuga.",
+            "epost_rubrik": "Påminnelse om kodstuga",
+            "epost_status": "aktiv",
+            "epost_text_ja": "Välkommen",
+            "epost_rubrik_ja": "Du har nu tackat ja.",
+            "epost_status_ja": "aktiv",
+            "typ": "Kodstuga",
+            "open": "Ja"
+        },
+        "volontar": {
+            "namn": "Test Volontär",
+            "epost": uuid4().hex + "@none.com",
+            "telefon": "07231" + str(randint(10000, 99990)),
+            "kodstugor_id": None,
+            "roller": "volontär"
+        },
+        "datum": {
+            "kodstugor_id": None,
+            "datum": [
+                arrow.utcnow().to('Europe/Stockholm').format('YYYY-MM-DD'),
+                arrow.utcnow().shift(hours=24).to('Europe/Stockholm').format('YYYY-MM-DD'),
+                arrow.utcnow().shift(hours=48).to('Europe/Stockholm').format('YYYY-MM-DD')
+            ],
+            "typ": [
+                "kodstuga",
+                "kodstuga",
+                "kodstuga"
+            ]
+        },
+        "utskick": {
+            "kodstugor_id": None,
+            "datum": arrow.utcnow().format('YYYY-MM-DD'),
+            "typ": "e-post",
+            "status": "aktiv",
+            "rubrik": "TEST_ATT_RADERA",
+            "text": "Meddelnande som skickas till kontaktpersonen detta datum."
+        },
+        "kontaktperson": {
+            "fornamn": "KP Fornamn",
+            "efternamn": "KP Efternamn",
+            "epost": uuid4().hex + "@test.com",
+            "telefon": "07231" + str(randint(10000, 99990))
+        },
+        "deltagare": {
+            "fornamn": "DT Fornamn",
+            "efternamn": "DT Efternamn",
+            "status": "ja",
+            "kon": "han",
+            "foto": "ja",
+            "klass": "åk 4",
+            "skola": "Stenåken",
+            "kodstuga": None,
+            "skonto": "",
+            "slosen": ""
+        },
+        "apply": {
+            "kodstuga": None,
+            "barn_fornamn": ["Test_deltagare_to_be_deleted", "Test2_deltagare_to_be_deleted"],
+            "barn_efternamn": ["Thuresson", "test1a"],
+            "klass": ["åk 5", "åk 4"],
+            "skola": ["Test", "Test"],
+            "kon": ["hon", "hen"],
+            "vuxen_fornamn": ["sdfsdf", "jh"],
+            "vuxen_efternamn": ["vbhjh", "vbhjh"],
+            "email": [uuid4().hex + "@test.com", uuid4().hex + "@test.com"],
+            "telefon": ["07231" + str(randint(10000, 99990)), "07231" + str(randint(1000, 99990))],
+            "hittade": "id2",
+            "approve": "ja"
+        },
+        "apply_admin": {
+            "kodstuga": None,
+            "barn_fornamn": ["Test Deltagare Förnamn", "Test Deltagare Förnamn 2"],
+            "barn_efternamn": ["Test Deltagare Efternamn", "Test Deltagare Efternamn 2"],
+            "klass": ["åk 5", "åk 4"],
+            "skola": ["Test", "Test"],
+            "foto": ["ja", "nej"],
+            "kon": ["hon", "hen"],
+            "vuxen_fornamn": ["sdfsdf", "jh"],
+            "vuxen_efternamn": ["vbhjh", "vbhjh"],
+            "email": [uuid4().hex + "@test.com", uuid4().hex + "@test.com"],
+            "telefon": ["07231" + str(randint(10000, 99990)), "07231" + str(randint(1000, 99990))],
+            "hittade": "id2",
+            "approve": "ja",
+            "invite_now": "ja"
+        },
+        "volontarer_plannering": {
+            "volontarer_id": None,
+            "kodstugor_id": None,
+            "datum": arrow.utcnow().format('YYYY-MM-DD'),
+            "status": "ja",
+            "kommentar": "Meddelande"
+        }
+    }
+    return {**post_typer[namn], **extra_data}
--- tests/test_kodstugor_datum.py
+++ tests/test_kodstugor_datum.py
@@ -3,7 +3,7 @@
 from managedata import datum
 
 
-def test_add_send_delete_email(as_admin, ny_kodstuga):
+def test_send_email_reminder(as_admin, ny_kodstuga):
     id_ = ny_kodstuga["id"]
     datum.send_email_reminders_once()
     kontakptersoner = as_admin.get(
@@ -15,17 +15,55 @@ def test_add_send_delete_email(as_admin, ny_kodstuga):
     found = False
     for kontakperson in kontakptersoner:
         if kontakperson["kodstugor_id"] == id_:
-
-            def hitta(listrad):
-                return kontakperson["epost"] in listrad
-            assert len(list(filter(hitta, emails))) == 1
+            assert len([row for row in emails if kontakperson["epost"] in row]) == 1
             found = True
+            break
     assert found
 
 
-def pest_add_send_delete_sms(as_admin, ny_kodstuga):
+def test_send_email_not_activate(as_admin, ny_kodstuga):
     id_ = ny_kodstuga["id"]
+    update_data = {
+        "id": id_,
+        "namn": "Test Kodstuga",
+        "sms_text": "Påminnelse om kodstuga.",
+        "sms_status": "aktiv",
+        "epost_text": "Påminnelse om kodstuga.",
+        "epost_rubrik": "Påminnelse om kodstuga",
+        "epost_status": "inaktiv",
+        "epost_text_ja": "Välkommen",
+        "epost_rubrik_ja": "Du har nu tackat ja.",
+        "epost_status_ja": "aktiv",
+        "typ": "Kodstuga",
+        "open": "Ja"
+    }
+    as_admin.post(
+        "http://127.0.0.1:9292/api/kodstugor",
+        data=update_data
+    )
+
+    datum.send_email_reminders_once()
+
+    kontakptersoner = as_admin.get(
+        "http://127.0.0.1:9292/api/kontaktpersoner"
+    ).json()["kontaktpersoner"]
+    emails = as_admin.get(
+        "http://127.0.0.1:9292/api/loggar?log=epost"
+    ).json()["logdata"]
+    found = False
+    for kontakperson in kontakptersoner:
+        if kontakperson["kodstugor_id"] == id_:
+            assert len([row for row in emails if kontakperson["epost"] in row]) == 0
+            found = True
+            break
+    assert found
+
+
+def test_sms_reminder(as_admin, ny_kodstuga):
+    id_ = ny_kodstuga["id"]
+
     datum.send_sms_reminders_once()
+
     kontakptersoner = as_admin.get(
         "http://127.0.0.1:9292/api/kontaktpersoner"
     ).json()["kontaktpersoner"]
@@ -35,9 +73,45 @@ def pest_add_send_delete_sms(as_admin, ny_kodstuga):
     found = False
     for kontakperson in kontakptersoner:
         if kontakperson["kodstugor_id"] == id_:
-
-            def hitta(listrad):
-                return kontakperson["telefon"] in listrad
-            assert len(list(filter(hitta, sms))) == 1
+            assert len([row for row in sms if kontakperson["telefon"] in row]) == 1
             found = True
+            break
+    assert found
+
+
+def test_sms_reminder_not_activate(as_admin, ny_kodstuga):
+    id_ = ny_kodstuga["id"]
+    update_data = {
+        "id": id_,
+        "namn": "Test Kodstuga",
+        "sms_text": "Påminnelse om kodstuga.",
+        "sms_status": "inaktiv",
+        "epost_text": "Påminnelse om kodstuga.",
+        "epost_rubrik": "Påminnelse om kodstuga",
+        "epost_status": "aktiv",
+        "epost_text_ja": "Välkommen",
+        "epost_rubrik_ja": "Du har nu tackat ja.",
+        "epost_status_ja": "aktiv",
+        "typ": "Kodstuga",
+        "open": "Ja"
+    }
+    as_admin.post(
+        "http://127.0.0.1:9292/api/kodstugor",
+        data=update_data
+    )
+
+    datum.send_sms_reminders_once()
+
+    kontakptersoner = as_admin.get(
+        "http://127.0.0.1:9292/api/kontaktpersoner"
+    ).json()["kontaktpersoner"]
+    sms = as_admin.get(
+        "http://127.0.0.1:9292/api/loggar?log=sms"
+    ).json()["logdata"]
+    found = False
+    for kontakperson in kontakptersoner:
+        if kontakperson["kodstugor_id"] == id_:
+            assert len([row for row in sms if kontakperson["telefon"] in row]) == 0
+            found = True
+            break
     assert found
